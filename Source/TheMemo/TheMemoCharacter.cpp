2423ae57a258b101d092c8bb7260a2e98e8fc9f5
ttofff <255374409@qq.com>
2024-07-25 00:11:22 +0800

    添加注释

--- Source/TheMemo/TheMemoCharacter.cpp
+++ Source/TheMemo/TheMemoCharacter.cpp
@@ -51,7 +51,7 @@ ATheMemoCharacter::ATheMemoCharacter()
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
 }
-
+//获取当前星期几
 FString ATheMemoCharacter::GetDayOfWeek()
 {
 	FDateTime Time = FDateTime::Now();
